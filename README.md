3799ffedaf8c679bed4b105e764acee9a8d91026
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-09-08 15:02:15 +0200

    initiated project and setup worker scripts

---
+++ README.md
@@ -0,0 +1,13 @@
+## Proxying Cloudflare Pages API requests
+
+Cloudflare's API can not be accessed in the browser because of missing CORS headers. This Proxy-Worker Script solves the issue and passthroughs all requests that are necessary for using the [sanity-plugin-cloudflare-pages-deploy](https://github.com/estallio/sanity-plugin-cloudflare-pages-deploy) plugin.
+
+The code in this repo was heavily inspired by the example in the Cloudflare docs [here](https://developers.cloudflare.com/workers/examples/cors-header-proxy).
+
+### Deployment
+You have three possibilities to deploy this script to Cloudflare Workers.
+1. Copy the code from `index.js` and past it into the Cloudflare Workers Web Editor.
+2. Install Cloudflare's CLI tool wrangler globally, login and deploy your script like [here](https://developers.cloudflare.com/workers/get-started/guide).
+3. Inject this project into a CI/CD pipeline, inject the wrangler ENV variables `CF_ACCOUNT_ID`, `CF_ZONE_ID` and `CF_API_TOKEN` respectively and run the `yarn publish` command.
+
+It is also possible to configure the associated domain via wrangler or the `wrangler.toml` file like it's documented [here](https://developers.cloudflare.com/workers/get-started/guide#optional-configure-for-deploying-to-a-registered-domain).
---
+++ index.js
@@ -0,0 +1,112 @@
+// allowed options http headers
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE',
+  'Access-Control-Max-Age': '86400'
+}
+
+const API_URL = 'https://api.cloudflare.com'
+
+// restrict API access to deployments and accounts listings to get the account id
+// -> this restriction should partly prevent abuse if credentials are leaked over sanity in the browser
+//    or someone is able to inject proper code into the sanity studio
+//    this has no effect if credentials are leaked as one could simply use e.g. postman or some self written code to do the requests
+// according to this, following urls are fine:
+// - https://api.cloudflare.com/client/v4/accounts/sfjhfof8u79s7df/pages/projects/some-project/deployments... - for deployment manipulation
+// - https://api.cloudflare.com/client/v4/accounts - to get the account id from email and api-key
+// If we assume the incoming URL can have some subdomain or subpath before, we check if the end of the paths matches the following regular expression:
+const allowedPathsRegex = new RegExp(/\/client\/v4\/accounts(\/[A-Za-z0-9_.-]+\/pages\/projects\/[A-Za-z0-9_.-]+\/deployments(\/projects\/[A-Za-z0-9_.-]+)?)?$/)
+
+function handleOptions (request) {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  let headers = request.headers
+
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    // If you want to check or reject the requested method + headers
+    // you can do that here.
+    let respHeaders = {
+      ...corsHeaders,
+      // Allow all future content Request headers to go back to browser
+      // such as Authorization (Bearer) or X-Client-Name-Version
+      'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers')
+    }
+
+    return new Response(null, {
+      headers: respHeaders
+    })
+  } else {
+    // Handle standard OPTIONS request.
+    // If you want to allow other HTTP Methods, you can do that here.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, HEAD, POST, OPTIONS'
+      }
+    })
+  }
+}
+
+async function handleRequest (request) {
+  const url = new URL(request.url)
+
+  // Rewrite request to point to API url. This also makes the request mutable
+  // so we can add the correct Origin header to make the API server think
+  // that this request isn't cross-site.
+  request = new Request(API_URL + url.pathname, request)
+  request.headers.set('Origin', new URL(API_URL).origin)
+  let response = await fetch(request)
+
+  // Recreate the response so we can modify the headers
+  response = new Response(response.body, response)
+
+  // Set CORS headers
+  response.headers.set('Access-Control-Allow-Origin', '*')
+
+  // Append to/Add Vary header so browser will cache response correctly
+  response.headers.append('Vary', 'Origin')
+
+  return response
+}
+
+addEventListener('fetch', event => {
+  const request = event.request
+
+  const url = new URL(request.url)
+
+  // only passthrough accounts and deployments endpoints
+  if (url.pathname.match(allowedPathsRegex)) {
+    // OPTIONS necessary for CORS check - browser do this first
+    if (request.method === 'OPTIONS') {
+      // Handle CORS preflight requests
+      event.respondWith(handleOptions(request))
+    } else if (
+      request.method === 'GET' ||
+      request.method === 'DELETE' ||
+      request.method === 'POST'
+    ) {
+      // Handle requests to the API server
+      event.respondWith(handleRequest(request))
+    } else {
+      // other methods are not allowed
+      event.respondWith(
+        new Response(null, {
+          status: 405,
+          statusText: 'Method Not Allowed'
+        })
+      )
+    }
+  } else {
+    // all other API requests are forbidden
+    event.respondWith(
+      new Response(null, {
+        status: 403,
+        statusText: 'Forbidden'
+      })
+    )
+  }
+})
